44b78b5d84588c1dff4e8890af2b55a4f954c78b
Rishi K <Rishi.K@iiitb.ac.in>
2026-07-15 12:33:12 +0530

    Update build_sim.sh

--- scripts/build_sim.sh
+++ scripts/build_sim.sh
@@ -3,6 +3,9 @@
 # Exit on any error
 set -e
 
+# Force execution from the root of the repository
+cd "$(dirname "$0")/.."
+
 echo -e "\033[36m[System] Cleaning previous builds...\033[0m"
 rm -rf obj_dir
 
@@ -10,14 +13,14 @@ echo -e "\033[36m[System] Compiling SiliconVision RTL Suite...\033[0m"
 
 verilator -Wall -Wno-WIDTHEXPAND -Wno-WIDTHTRUNC -Wno-PINCONNECTEMPTY -Wno-DECLFILENAME -Wno-GENUNNAMED -Wno-UNUSEDSIGNAL -Wno-PINMISSING \
     --cc \
-    sv_soc.v \
-    sys_array_blur.v \
-    sys_array_edge.v \
-    hist_eq.v \
-    dither_hw.v \
-    jpeg_encoder_hw.v rgb_ycbcr.v quantizer.v kernel_row_dct.v pe_dct.v sys_matrix.v reg.v skew_buff.v \
+    rtl/sv_soc.v \
+    rtl/sys_array_blur.v \
+    rtl/sys_array_edge.v \
+    rtl/hist_eq.v \
+    rtl/dither_hw.v \
+    rtl/jpeg_encoder_hw.v rtl/rgb_ycbcr.v rtl/quantizer.v rtl/kernel_row_dct.v rtl/pe_dct.v rtl/sys_matrix.v rtl/reg.v rtl/skew_buff.v \
     --top-module sv_soc \
-    --exe main_sim.cpp \
+    --exe host/main_sim.cpp \
     -CFLAGS "$(pkg-config --cflags opencv4) -O3" \
     -LDFLAGS "$(pkg-config --libs opencv4) -ljpeg" \
     --build
